ed3953a60f5ac48977168013938871aad15249e3
Wangari-j <wangarijudy@gmail.com>
2024-02-20 21:20:06 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def greet(): #def: keyword used to create a function #greet - name of the function
+    print("Hello World!") #function body
+
+greet() #calling function
+
+print("Karibu Kenya")
+
+
+
+
+
